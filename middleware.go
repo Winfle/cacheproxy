125a650eb93be6d9401e42f6f9ffc630337f3020
Andriy Kravets <andrii.kravets@bhn.com>
2024-09-09 18:40:15 +0200

    wip

--- middleware.go
+++ middleware.go
@@ -2,19 +2,28 @@ package middleware
 
 import (
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 const PluginName = "middleware"
 
+var logger *zap.Logger
+
 type Plugin struct{}
 
 func (p *Plugin) Init() error {
+	logger := zap.Must(zap.NewProduction())
+
+	defer logger.Sync()
+
+	logger.Info("Hello from Zap logger!")
 	return nil
 }
 
 func (p *Plugin) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// p.log.Info("Request")
+		logger.Info("Request")
 
 		next.ServeHTTP(w, r)
 	})
